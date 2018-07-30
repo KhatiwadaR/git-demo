1dd409f32ada7faa75b785f49c683d71b208d3f9
KhatiwadaR <rajeshkhatiwada99@gmail.com>
2018-07-30 08:04:39 +0545

    commit message

--- Helloworld.java
+++ Helloworld.java
@@ -1,3 +1,14 @@
-public class Helloworld{
-	public static void main(string[] args)
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class HelloWorld extends HttpServlet{
+
+	public void doGet(HttpServletRequest request,HttpServletResponse response)
+	throws IOException,ServletException{
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("Hello World!");
+
+
+	}
 }
